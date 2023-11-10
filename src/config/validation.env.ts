51220e332c33ee18c6d6d7bb5cd505bd7cdba3a4
Tobias duelli <duellit@gmail.com>
2023-11-10 10:06:25 +0100

    changed port

--- src/config/validation.env.ts
+++ src/config/validation.env.ts
@@ -2,5 +2,5 @@ import * as Joi from 'joi';
 
 export const envValidation = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production').required(),
-  PORT: Joi.number().default(3000),
+  PORT: Joi.number().default(9000),
 });
--- src/main.ts
+++ src/main.ts
@@ -49,6 +49,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(9000);
 }
 bootstrap();
